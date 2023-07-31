2e18893d6970e48ce9339e45a5a59811b19b2b34
eobeans <516225899@qq.com>
2023-07-31 16:00:32 +0800

    添加注释，pnpm安装server

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import type { AxiosProgressEvent, GenericAbortSignal } from 'axios'
 import { post } from '@/utils/request'
 import { useAuthStore, useSettingStore } from '@/store'
 
+// 访问chatApi
 export function fetchChatAPI<T = any>(
   prompt: string,
   options?: { conversationId?: string; parentMessageId?: string },
@@ -14,12 +15,14 @@ export function fetchChatAPI<T = any>(
   })
 }
 
+// 获取用户配置
 export function fetchChatConfig<T = any>() {
   return post<T>({
     url: '/config',
   })
 }
 
+// 通过api访问chatGPT
 export function fetchChatAPIProcess<T = any>(
   params: {
     prompt: string
@@ -52,12 +55,14 @@ export function fetchChatAPIProcess<T = any>(
   })
 }
 
+// 获取用户会话
 export function fetchSession<T>() {
   return post<T>({
     url: '/session',
   })
 }
 
+// 验证用户权限
 export function fetchVerify<T>(token: string) {
   return post<T>({
     url: '/verify',
@@ -65,12 +70,13 @@ export function fetchVerify<T>(token: string) {
   })
 }
 
+// 获取用户token
 export function fetchTooken<T>(params: {
   username: string
   password: string
   }) {
   return post<T>({
-    url: '/eobeans/token',
+    url: '/token',
     data: { params },
   })
 }
